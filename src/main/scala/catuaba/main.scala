37b5d8df74f5725ffa2e0bb30f3fcb978a72c896
Guilherme Bertuol <guilherme.bertuol@gmail.com>
2019-10-06 19:12:57 +0100

    sorted out publication

--- src/main/scala/catuaba/main.scala
+++ src/main/scala/catuaba/main.scala
@@ -5,21 +5,24 @@ import doobie.implicits._
 import doobie.util.ExecutionContexts
 import doobie.postgres._
 import doobie.free.connection.unit
+import doobie.util.transactor.Strategy
 import cats._
 import cats.effect._
 import cats.implicits._
 import fs2.Stream
 import org.postgresql.PGProperty
-import doobie.util.transactor.Strategy
 import org.postgresql.replication.LogSequenceNumber
+import java.util.concurrent.TimeUnit
 
 object Main extends IOApp {
   import Queries._
 
   def initReplicationSlot = {
     for {
-      slotInfo <- getSlotInfo
-      _        <- putStrLn(show"~> $slotInfo")
+      serverIdentity <- identifySystem
+      _              <- putStrLn(show"~> $serverIdentity")
+      slotInfo       <- getSlotInfo
+      _              <- putStrLn(show"~> $slotInfo")
       startPosition <- slotInfo match {
         case None =>
           for {
@@ -27,15 +30,37 @@ object Main extends IOApp {
             _           <- putStrLn(show"~> $createdSlot")
           } yield createdSlot.walStartLSN
         case Some(si) =>
-          FC.pure(si.latestFlushedLSN)
+          val resolvedPos =
+            if (si.confirmedFlushLSN.compareTo(serverIdentity.walFlushLocationLSN) < 0)
+              serverIdentity.walFlushLocationLSN
+            else
+              si.confirmedFlushLSN
+          FC.pure(resolvedPos)
       }
     } yield startPosition
+  }
+
+  def initPublication = {
+    for {
+      nrOfPublications <- countOfPublications
+      _ <- if (nrOfPublications == 0L) {
+        putStrLn("creating new publication") *> createPublication
+      } else {
+        putStrLn("reusing publication")
+      }
+    } yield ()
   }
 
   def startStreaming = {
     for {
       startPos <- initReplicationSlot
       _        <- putStrLn(s"startPos = $startPos")
+      _        <- initPublication
+      _ <- startReplicationStream(startPos).use { pgStream =>
+        for {
+          _ <- putStrLn(s"initializing stream ${pgStream.getLastReceiveLSN()}")
+        } yield ()
+      }
     } yield ()
   }
 
@@ -47,7 +72,7 @@ object Main extends IOApp {
   // 3. start streaming
 
   val props = new java.util.Properties()
-  PGProperty.USER.set(props, "repl_user")
+  PGProperty.USER.set(props, "postgres")
   PGProperty.PASSWORD.set(props, "docker")
   PGProperty.ASSUME_MIN_SERVER_VERSION.set(props, "11")
   PGProperty.REPLICATION.set(props, "database")
@@ -67,17 +92,18 @@ object Main extends IOApp {
   }
 
   object Queries {
-    val slotName = "test_slot"
-    val database = "world"
-    val plugin   = "pgoutput"
+    val slotName             = "test_slot"
+    val database             = "world"
+    val plugin               = "pgoutput"
+    val publicationName      = "test_publication"
+    val statusUpdateInterval = 10
 
     implicit val lsnMeta: Meta[LogSequenceNumber] = Meta[String]
       .imap(LogSequenceNumber.valueOf(_))(_.asString())
 
     case class SlotInfo(active: Boolean, confirmedFlushLSN: LogSequenceNumber, restartLSN: LogSequenceNumber, xmin: Option[String]) {
-      val latestFlushedLSN = confirmedFlushLSN.asLong()
+      val latestFlushedLSN = confirmedFlushLSN
     }
-
     object SlotInfo {
       implicit val show: Show[SlotInfo] = Show.fromToString
     }
@@ -92,7 +118,7 @@ object Main extends IOApp {
       statement.query[SlotInfo].option
     }
 
-    case class SlotCreated(slotName: String, consistentPoint: Long, snapshotName: String, outputPlugin: String) {
+    case class SlotCreated(slotName: String, consistentPoint: LogSequenceNumber, snapshotName: String, outputPlugin: String) {
       val walStartLSN = consistentPoint
     }
     object SlotCreated {
@@ -109,7 +135,46 @@ object Main extends IOApp {
             // .withTemporaryOption()
             .make()
         )
-      } yield SlotCreated(info.getSlotName(), info.getConsistentPoint().asLong(), info.getSnapshotName(), info.getOutputPlugin())
+      } yield SlotCreated(info.getSlotName(), info.getConsistentPoint(), info.getSnapshotName(), info.getOutputPlugin())
+    }
+
+    def startReplicationStream(startPos: LogSequenceNumber) = {
+      val stream = for {
+        replicationAPI <- PHC.pgGetConnection(PFPC.getReplicationAPI)
+        replStream <- FC.delay {
+          replicationAPI
+            .replicationStream()
+            .logical()
+            .withSlotName(slotName)
+            .withStartPosition(startPos)
+            .withSlotOption("proto_version", 1)
+            .withSlotOption("publication_names", publicationName)
+            .withStatusInterval(statusUpdateInterval, TimeUnit.SECONDS)
+            .start()
+        }
+      } yield replStream
+
+      Resource.make(stream)(s => FC.delay(s.close()))
+    }
+
+    case class ServerIdentity(systemId: String, xlogpos: String) {
+      lazy val walFlushLocationLSN = LogSequenceNumber.valueOf(xlogpos.toLong)
+    }
+    object ServerIdentity {
+      implicit val show: Show[ServerIdentity] = Show.fromToString
+    }
+
+    def identifySystem: ConnectionIO[ServerIdentity] = {
+      val statement = sql"IDENTIFY_SYSTEM"
+      statement.query[ServerIdentity].unique
+    }
+
+    def countOfPublications: ConnectionIO[Long] = {
+      sql"select count(1) from pg_publication where pubname = $publicationName".query[Long].unique
+    }
+
+    def createPublication: ConnectionIO[Unit] = {
+      Update0(s"create publication $publicationName for all tables", None).run.void
     }
   }
 
